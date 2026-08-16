403b12917166e3a36aeddc4346e6fffac6822b0f
kanika-drouna02 <duskybook02@gmail.com>
2026-08-16 16:56:21 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (95.57%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
-        priority_queue<int>pq;
-        int n=matrix.size(), m=matrix[0].size();
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                pq.push(matrix[i][j]);
-                if(pq.size()>k) pq.pop();
+    int countSmall(vector<vector<int>>& nums, int val){
+        int n=nums.size(), m=nums[0].size();
+        int count=0;
+        for(int i=0;i<n;i++)
+        count += upper_bound(nums[i].begin(), nums[i].end(), val) - nums[i].begin();
+        return count;
+    }
+
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n= matrix.size();
+        int m=matrix[0].size();
+        int l=matrix[0][0], r=matrix[n-1][m-1];
+        int ans=-1;
+        while(r>=l){
+            int mid= l+(r-l)/2;
+            int count = countSmall(matrix,mid);
+            if(count<k) l=mid+1;
+            else {
+                ans = mid;
+                r=mid-1;
             }
         }
-        return pq.top();
+        return ans;
     }
 };
