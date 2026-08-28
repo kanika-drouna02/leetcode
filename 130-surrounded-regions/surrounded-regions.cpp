a2a0f2cb3a0ccd634d460a669655dd605bedc58f
kanika-drouna02 <duskybook02@gmail.com>
2026-08-29 03:28:48 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (23.16%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    void solve(vector<vector<char>>& board) {
+        int n= board.size(), m=board[0].size();
+        queue<pair<int,int>>q;
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        for(int i=0;i<n;i++) {
+            if(board[i][0]=='O' ) {
+                q.push({i,0});
+                vis[i][0]=1;  
+            }
+            if(board[i][m-1]=='O') {
+                q.push({i,m-1});
+                vis[i][m-1]=1;
+            }
+        }
+        for(int i=0;i<m;i++) {
+            if(board[0][i]=='O') {
+                q.push({0,i});
+                vis[0][i]=1;
+            }
+            if(board[n-1][i]=='O') {
+                q.push({n-1,i});
+                vis[n-1][i]=1;
+            }
+        }
+
+        int dr[]={-1,0,1,0};
+        int dc[]={0,1,0,-1};
+
+        while(!q.empty()){
+            int r=q.front().first;
+            int c=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nr=r+dr[i];
+                int nc=c+dc[i];
+                if(nr>=0 && nr<n && nc>=0 && nc<m && vis[nr][nc]==0 && board[nr][nc]=='O'){
+                    q.push({nr,nc});
+                    vis[nr][nc]=1;
+                }
+            }
+        } 
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='O' && vis[i][j]==0) board[i][j]='X';
+            }
+        }
+    }
+};
